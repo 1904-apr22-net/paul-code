7252d5f62fb39e72d49c950ef01590e64b623216
Paul <paul_grimes8@yahoo.com>
2019-04-29 11:48:12 -0500

    test

---
+++ Assessment Week 1/String.Utility/String.Utility/StringFunctions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace String.Utility
+{
+    public class StringFunctions
+    {
+        public bool Palindrome(string word)
+        {
+            bool test = true;
+            char[] reversed = word.ToCharArray();
+            Array.Reverse(reversed);
+            string RString = new string(reversed);
+            int count = 0;
+            for (int i = 0; i < word.Length/2; i++)
+            {
+                while(char.IsLetterOrDigit(word[i]) == false)
+                {
+                    i++;
+                }
+                if (char.IsLetterOrDigit(word[i]))
+                {
+                    if (char.IsLetterOrDigit(RString[word.Length - 1 - count]))
+                    {
+                        if (char.ToLower(word[i]) != char.ToLower(RString[word.Length - 1 - count]))
+                        {
+                            test = false;
+                            char a = word[i];
+                            char b = RString[word.Length - 1 - count]; 
+                        }
+                    }
+                    
+                    count++;
+
+                }
+                
+            }
+            return test;
+        }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StringFunctions f = new StringFunctions();
+            //f.Palindrome("Paul");
+            Console.WriteLine(f.Palindrome("Paul"));
+        }
+    }
+}
---
+++ Assessment Week 1/String.Utility/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using String.Utility;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+            StringFunctions Test = new StringFunctions();
+            bool bTest;
+            //act
+            bTest = f.Palindrome("pap");
+            //assert
+            Assert(bTest);
+        }
+    }
+}
